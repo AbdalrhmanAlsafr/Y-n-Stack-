0d6ed69f4b5cf58f364d6b7992d47a81c8ac5977
Abdalrhman Alsafr <44406952+AbdalrhmanAlsafr@users.noreply.github.com>
2018-12-22 17:41:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Stack dizi = new Stack();
+            dizi.Push(1);
+            dizi.Push(2);
+            dizi.Push(3);
+            
+            foreach (int i in dizi)
+                Console.WriteLine(i);
+
+            dizi.Pop();
+       
+            foreach (int i in dizi)
+                Console.WriteLine(i);
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+    }
+}
